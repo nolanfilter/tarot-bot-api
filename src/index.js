7c9c4611796a1ab6c4ba0ce729f03c5bb4a19eb6
Nolan Filter <nolan.f.filter@gmail.com>
2024-10-19 11:20:55 -0400

    Fix CSS bug

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ let tb_images = JSON.parse( tb_image_urls )
 
 // console.log( fakeurl )
 
-app.use( '/css', express.static( path.join( __dirname, '../public/css' ) ) )
+app.use( '/css', express.static( path.join( __dirname, './css' ) ) )
 
 app.use( '/favicon.ico', express.static( path.join( __dirname, '../public/images/favicon.ico' ) ) )
 app.use( '/favicon.png', express.static( path.join( __dirname, '../public/images/favicon.png' ) ) )
